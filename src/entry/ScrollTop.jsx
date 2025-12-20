4ace143f5ec8a6b50befb5bcb660766d8a1d4944
OakLeaf007 <jjjbbbb736@gmail.com>
2025-12-20 22:47:36 +0530

    update 1.2/major ui bug fix

--- src/entry/ScrollTop.jsx
+++ src/entry/ScrollTop.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollTop(){
+export default function ScrollTop({ scrollRef }){
 
     const {pathname} = useLocation();
 
@@ -9,6 +9,9 @@ export default function ScrollTop(){
         const main = document.querySelector("main");
        if (main) main.scrollTop = 0;
         // window.scrollTo(0,0);
+        // if(scrollRef?.current){
+        //     scrollRef.current.ScrollTop = 0;
+        // }
     }, [pathname]);
     return null;
 }
