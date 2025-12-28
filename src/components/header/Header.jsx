01239f9769ff4c5ca448fc7754f21478b3dd0ffe
OakLeaf007 <jjjbbbb736@gmail.com>
2025-12-28 19:22:06 +0530

    review ui update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -47,7 +47,7 @@ useEffect(()=>{
             {/* these are the navbar elements with anchor tag */}
             <li className="nav-item  nav-li"><Link to="/signin" className="nav-link p-2" >signin</Link></li>
             <li className="nav-item   nav-li"><Link to ="/signup"  className="nav-link p-2 ">signup</Link></li> <div id="seperator"></div>
-            <li className="nav-item   nav-li d-flex gap-0 px-1"><span className="nav-link p-2 d-flex gap-1 span-nav" href="null"><i className="fa-solid fa-user"></i></span></li>
+            <li className="nav-item   nav-li d-flex gap-0 px-1"><span className="nav-link p-2 d-flex gap-1 span-nav" href="null"><div id="account-id"><i className="fa-solid fa-user"></i></div></span></li>
             
           </ul>
         </div>
--- src/components/product/ReviewSection.jsx
+++ src/components/product/ReviewSection.jsx
@@ -1,12 +1,58 @@
 import "./review.css"
 
+import RatingStars from "./Rating"
+
 export default function Review(){
     return(
         <div id="rating-parent">
-            <div id="rating">
+            <div id="rating-cont">
+                     <div id="rating">
                 <span id="rating-num">5</span>
                  <span><i className="fa-solid fa-star"></i></span> 
+
+                     
                  </div>
+                 <div>
+                    <span style={{fontSize:"16px", color:"#878787"}}>69 user ratings</span>
+                 </div>
+                
+                <div><button id="rate-btn" >Rate</button></div>
+                </div>
+
+            <div>
+                <div className="review-imgs">
+                    <span>Images uploaded by customers:</span>
+                    <div id="review-img-container">  
+                        <div className="review-img" >
+                            <img src="http://localhost:5173/product1.png"></img>
+                        </div>
+                    </div>
+                </div>
+                    <div className="user-review">
+                  <span className="user-name">oak</span>
+                    <span clasName="user-star"><RatingStars /></span>
+                    <div className="user-uploaded-img">
+                        <div className="user-img">
+                            <img src="http://localhost:5173/product1.png"></img>
+                        </div>
+                    </div>
+                    <p  className="review-para">Good quality product used for 69 days still going good</p>
+
+                </div>
+
+                    <div className="user-review">
+                  <span className="user-name">Langra Penguin</span>
+                    <span clasName="user-star"><RatingStars /></span>
+                    <div className="user-uploaded-img">
+                        <div className="user-img">
+                            <img src="http://localhost:5173/product1.png"></img>
+                        </div>
+                    </div>
+                    <p  className="review-para">My wife didnt like it..fitting is not up to the mark</p>
+
+                </div>
+            </div>
+           
         </div>
     )
 }
