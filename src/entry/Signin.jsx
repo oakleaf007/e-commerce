f5f035350b9e5151962f4c60904ace7a4d38f64e
OakLeaf007 <jjjbbbb736@gmail.com>
2026-03-13 13:10:35 +0530

    feat: signup & signin funtinality

--- src/entry/Signin.jsx
+++ src/entry/Signin.jsx
@@ -1,19 +1,69 @@
 import "./signin.css";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+
+
+import { useState } from "react";
+
+
 export default function Signin(){
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        email:"",
+        password: ""
+    });
+    const [message, setMessage] = useState({
+        text: "",
+        type: ""
+    });
+
+    function handleChange(e){
+        setForm({
+            ...form,
+           [e.target.name]: e.target.value
+        });
+    }
+
+ async function handleSubmit(e){
+        e.preventDefault();
+
+        try{
+            const res = await fetch("http://localhost:3000/api/signin",{
+                method: "POST",
+                headers:{"Content-Type": "application/json"},
+                body: JSON.stringify(form)
+
+            });
+
+            const data = await res.json();
+            if(res.ok){
+                setMessage({text: data.message, type: "success"});
+                localStorage.setItem("token", "true");
+                navigate("/");
+            }
+            else{
+                setMessage({text: data.message, type: "error"});
+            }
+
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+
     return(
         <div id="form-parent">
             <div id="form-container">
                 <h1>Welcome Back</h1>
                 <h2>Please login to your account</h2>
-                <form>
-                    <input type="text" placeholder="email"/>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" placeholder="email" name="email" value={form.email} onChange={handleChange}/>
                   
-                    <input type="password" placeholder="password" />
+                    <input type="password" placeholder="password" name="password" value={form.password} onChange={handleChange}/>
                   
                    
 
                     <div id="check"> <input type="checkbox" /><span>Password visibility</span></div>
+                    <div id="message" className={message.type}>{message.text}</div>
                     <button>Login</button>
                     <span>Don't have an account? <Link to="/signup">Signup</Link></span>
                 </form>
